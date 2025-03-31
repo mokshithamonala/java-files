3771c35f7b53e22a3920c58911706f5b2408d1ed
mokshithamonala <saimokshitha478@gmail.com>
2025-03-31 22:25:22 +0530

    Update vehicle.java

--- java practice/vehicle.java
+++ java practice/vehicle.java
@@ -2,6 +2,7 @@ class Vehicle {
     public String car_model;
     public String color;
     public String fuel_type;
+    
     public Vehicle(String car_model, String color, String fuel_type) {
         this.car_model = car_model;
         this.color = color;
@@ -17,21 +18,23 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-    
     public Car(String car_model, String color, String fuel_type) {
         super(car_model, color, fuel_type);
     }
 
+    @Override
     void displayInfo() {
         super.displayInfo(); 
         System.out.println("I have a Benz car");
     }
 }
 
-class truck {
+public class Truck {
     public static void main(String[] args) {
         Vehicle v = new Vehicle("Audi", "Red", "Petrol");
         v.displayInfo();
         
+        Car c = new Car("Benz", "Black", "Diesel");
+        c.displayInfo();
     }
 }
