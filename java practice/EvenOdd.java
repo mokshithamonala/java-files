69eb9ade2aec4477205ba107dcdbc134a06993ab
mokshithamonala <saimokshitha478@gmail.com>
2025-04-03 00:21:34 +0530

    Update EvenOdd.java

--- java practice/EvenOdd.java
+++ java practice/EvenOdd.java
@@ -1,11 +1,17 @@
-class EvenOdd {
-    pubic static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int num=sc.nextInt();
-        if(numb%2==0){
-          System.out.println(num + "is an even number");
-         } else{
-           System.out.println(num+"is an odd number");
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number");
+        } else {
+            System.out.println(num + " is an odd number");
+        }
+        
+        sc.close();
     }
 }
