62e9c43c5b830fea1eccecf520a6533c4db383b4
mokshithamonala <saimokshitha478@gmail.com>
2025-04-02 12:25:01 +0530

    Add files via upload

---
+++ java practice/Fibonacci .java
@@ -0,0 +1,10 @@
+class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter no of terms:");
+        int n=sc.nextInt();
+        int a=0,b=1,next;
+        System.out.print("Fibonacci Series:"+a" "
+    }
+}
