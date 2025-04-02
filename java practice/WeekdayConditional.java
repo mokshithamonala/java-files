abefbf2f188138877d8ed5f59c2a5687c5a77d87
mokshithamonala <saimokshitha478@gmail.com>
2025-04-02 12:03:38 +0530

    Add files via upload

---
+++ java practice/WeekdayConditional.java
@@ -0,0 +1,31 @@
+class WeekdayConditional {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input number(1-7): ");
+        int numb=sc.nextInt();
+        String day;
+        if(num ==1) {
+            day="sunday";
+        }else if(num ==2) {
+            day="monday";
+        }else if (numb ==3) {
+            day="tueday";
+        }else if (numb ==4) {
+            day="wednesday";
+        }else if (numb ==5) {
+            day="thursday";
+        }else if (numb ==6) {
+            day ="friday";
+        }else{ 
+            day="saturday";
+             System.out.println("Day of the week is: "+day);
+        }
+                            
+                            
+                        
+                    
+                
+
+            
+        
+    
