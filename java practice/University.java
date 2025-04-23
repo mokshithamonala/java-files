1ca0b07c88f8e8eac7a5fac4dd1424cc0707e1d8
mokshithamonala <saimokshitha478@gmail.com>
2025-04-23 12:23:11 +0530

    Add files via upload

---
+++ java practice/University.java
@@ -0,0 +1,13 @@
+public class University {
+    String UniversityName="Amrita viswa vidyapeetam";
+    class Department{
+        String deptName="Computer Science";
+        int deptcode=213;
+        void displayDetails()
+        System.out.println("Department:" +deptName+"code"+dept code);
+        class student
+        String StudentName
+        int StudentId;
+        
+    }
+}
