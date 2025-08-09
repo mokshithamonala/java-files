ca980cb5d97acb0c1c458d86f4dd8b62d57226da
mokshithamonala <saimokshitha478@gmail.com>
2025-08-09 10:25:16 +0530

    Add files via upload

---
+++ pointer8.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+void main() {
+int arr[5]={10,20,30,40,50};
+int *ptr=arr;
+printf("Intial pointer value:\n");
+printf("Adress:%p,value:%d\n,ptr,*ptr");
+ptr--;
+printf("\nAfter incrementing pointer by 1:\n");
+printf("Adress:%p,Value:%d\n",ptr,*ptr);
+ptr--;
+printf("\nAfter incrementing pointer by 1 again:\n");
+printf("Adress:%p,value:%d\n",ptr,*ptr);
+}
